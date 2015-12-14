7c90b776e3bf9e052f11c3a12bc5ad578cb7dd1e
sxf <sun_xfancy@163.com>
2015-12-14 15:02:14 +0800

    添加两个安装脚本 负责将编译好的库安装到对应的目录下
    Signed-off-by: sxf <sun_xfancy@163.com>

---
+++ build_scripts/cp_unix.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# @Author: sxf
+# @Date:   2015-12-14 14:46:47
+# @Last Modified by:   sxf
+# @Last Modified time: 2015-12-14 14:59:10
+
+cd ..
+cp -r ./local_install/oolua ../../../extlib/include/
+cp ./local_install/*.a  ../../../extlib/lib/
---
+++ build_scripts/cp_win32.bat
@@ -0,0 +1,7 @@
+@REM @Author: sxf
+@REM @Date:   2015-12-14 14:47:12
+@REM @Last Modified by:   sxf
+@REM Modified time: 2015-12-14 15:01:16
+
+xcopy .\local_install\oolua ..\..\..\extlib\include\
+copy .\local_install\*.lib ..\..\..\extlib\lib\
